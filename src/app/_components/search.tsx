c9ad039ac1bcc9267f04758afa09b0e3ce873a8d
Reece Yang <reeceyang@live.com>
2024-11-08 14:08:39 -0500

    move meiToRegex into common folder and display match count in search results

--- src/app/_components/search.tsx
+++ src/app/_components/search.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import { api } from "@/trpc/react";
+import { meiToRegex } from "@/common/meiToRegex";
 
 export function Search() {
   const [input, setInput] = useState("");
@@ -37,15 +38,25 @@ export function Search() {
       </form>
       <div className="flex flex-col gap-6">
         <p>{data?.length ?? 0} results.</p>
-        {data?.length &&
-          data.map((score, i) => (
-            <div
-              className="rounded-md bg-white/10 p-3 backdrop-blur-md"
-              key={i}
-            >
-              <p>{score.title}</p>
-            </div>
-          ))}
+        {data?.length
+          ? data.map((score, i) => {
+              const regex = meiToRegex(meiText);
+              const numMatches = [
+                ...score.kernData.matchAll(new RegExp(regex, "g")),
+              ].length;
+              return (
+                <div
+                  className="flex flex-row rounded-md bg-white/10 p-3 backdrop-blur-md"
+                  key={i}
+                >
+                  <p>{score.title}</p>
+                  <p className="ml-auto">
+                    {numMatches} match{numMatches !== 1 && "es"}
+                  </p>
+                </div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
