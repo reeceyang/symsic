0b592aa797ba817aa07424fd76debe6bc516ac79
Reece Yang <reeceyang@live.com>
2024-11-12 21:34:47 -0500

    functional advanced search checkbox

--- src/app/_components/search.tsx
+++ src/app/_components/search.tsx
@@ -1,21 +1,43 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "@/trpc/react";
 import { patternToMEI } from "@/common/meiToRegex";
 import { Checkbox, Field, Label } from "@headlessui/react";
 
+const DEFAULT_INPUT = `<beam>
+    <note dur="8" dots="1" query:any-pitch="true" />
+    <note dur="16" query:any-pitch="true" />
+    <note dur="8" query:any-pitch="true" />
+</beam>`;
+
 export function Search() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(DEFAULT_INPUT);
   const [meiText, setMeiText] = useState(``);
   const { data } = api.search.search.useQuery(
     { meiText },
     { enabled: Boolean(meiText) },
   );
   const [svg, setSvg] = useState("");
+  const [isAdvancedSearch, setIsAdvancedSearch] = useState(true);
+
+  // HACK: wait for verovio to be loaded before rendering
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      const svg: string = verovioToolkit.renderData(
+        patternToMEI(input),
+        {},
+      );
+      setSvg(svg);
+    }, 500);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
 
   return (
-    <div className="flex w-full h-full max-w-md flex-col gap-6">
+    <div className="flex h-full w-full max-w-md flex-col gap-6">
       <div
         className="h-36 overflow-clip rounded-md bg-white"
         dangerouslySetInnerHTML={{ __html: svg }}
@@ -23,48 +45,53 @@ export function Search() {
       <div className="flex flex-row gap-4">
         <Field className="flex items-center gap-2">
           <Checkbox
-            checked={true}
+            checked={isAdvancedSearch}
+            onChange={() => setIsAdvancedSearch(!isAdvancedSearch)}
             className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
           />
           <Label>Advanced search</Label>
         </Field>
-        <Field className="flex items-center gap-2">
-          <Checkbox
-            checked={true}
-            className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
-          />
-          <Label>Use sequential scan</Label>
-        </Field>
+        {isAdvancedSearch && (
+          <Field className="flex items-center gap-2">
+            <Checkbox
+              checked={true}
+              className="group block size-4 rounded border bg-white data-[checked]:bg-blue-500"
+            />
+            <Label>Use sequential scan</Label>
+          </Field>
+        )}
       </div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          setMeiText(input);
-        }}
-        className="flex flex-row gap-2"
-      >
-        <textarea
-          placeholder="MEI"
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-            const svg: string = verovioToolkit.renderData(
-              patternToMEI(e.target.value),
-              {},
-            );
-            setSvg(svg);
+      {isAdvancedSearch && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setMeiText(input);
           }}
-          className="w-full rounded-md px-4 py-2 text-black"
-        />
-        <div>
-          <button
-            type="submit"
-            className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-          >
-            Search
-          </button>
-        </div>
-      </form>
+          className="flex flex-row gap-2"
+        >
+          <textarea
+            placeholder="MEI"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+              const svg: string = verovioToolkit.renderData(
+                patternToMEI(e.target.value),
+                {},
+              );
+              setSvg(svg);
+            }}
+            className="w-full rounded-md px-4 py-2 text-black"
+          />
+          <div>
+            <button
+              type="submit"
+              className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      )}
       <div className="flex flex-col gap-6 overflow-y-auto">
         <p>{data?.length ?? 0} results.</p>
         {data?.length
