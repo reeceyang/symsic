77673e8164a39388f7c5664602443f2c97332265
Reece Yang <reeceyang@live.com>
2024-12-08 01:14:11 -0500

    only include **kern voices

--- split-voices.ts
+++ split-voices.ts
@@ -6,27 +6,38 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function insertSplitVoices() {
-  const scores = await db.query.kernScores.findMany({ limit: 100 });
+  const scores = await db.query.kernScores.findMany();
 
   await db.transaction(async (tx) => {
-    await tx.insert(kernVoices).values(
-      scores.flatMap((score) => {
-        const kernStartIndex = score.kernData.indexOf("**kern");
-        const kernEndIndex = score.kernData.lastIndexOf("*-") + "*-".length;
+    let counter = 0;
+    for (const score of scores) {
+      counter++;
+      if (counter % 100 === 0) {
+        console.log(counter);
+      }
+      const kernStartIndex = score.kernData.indexOf("**kern");
+      const kernEndIndex = score.kernData.lastIndexOf("*-") + "*-".length;
 
-        const scoreData = score.kernData.slice(kernStartIndex, kernEndIndex);
-        const numVoices =
-          scoreData.match(/\*\*kern/)?.length ?? assert.fail("No voices found");
-        const voices = Array(numVoices).fill("");
+      const scoreData = score.kernData.slice(kernStartIndex, kernEndIndex);
+      const firstLine =
+        scoreData.split("\n")[0] ?? assert.fail("No first line found");
+      const columnHeaders = firstLine.split("\t");
+      const voices: string[] = Array(columnHeaders.length).fill("");
 
-        for (const line of scoreData.split("\n")) {
-          line.split("\t").forEach((token, i) => {
-            voices[i] += token + "\n";
-          });
-        }
-        return voices.map((voice) => ({ scoreId: score.id, voice }));
-      }),
-    );
+      for (const line of scoreData.split("\n")) {
+        line.split("\t").forEach((token, i) => {
+          voices[i] += token + "\n";
+        });
+      }
+
+      await tx
+        .insert(kernVoices)
+        .values(
+          voices
+            .filter((voice) => voice.startsWith("**kern"))
+            .map((voice) => ({ scoreId: score.id, voice })),
+        );
+    }
   });
 }
 
